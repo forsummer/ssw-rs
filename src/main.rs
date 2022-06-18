2ab19ad2cffa25ea7b94bf3a000e20a24547c890
forsummer <1321067378@qq.com>
2022-06-18 20:38:33 +0800

    Fix `smith_waterman_serial`,  let its computing result correct

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-// mod args;
-// mod load;
+mod args;
+mod load;
 mod utils;
-// mod pairwise;
+mod pairwise;
 
-// use clap::Parser;
+use clap::Parser;
 
-// use args::CliArgs;
-// use load::load_fastx;
+use args::CliArgs;
+use load::load_fastx;
+
 // use pairwise::smith_waterman_avx2;
+use pairwise::smith_waterman_serial;
 
 fn main()
 {
+    let cli_args = CliArgs::parse();
+    let d_set = load_fastx(cli_args.d_path);
+    let q_set = load_fastx(cli_args.q_path);
     
+    for d in d_set.iter()
+    {
+        for q in q_set.iter()
+        {
+            let match_ = cli_args._match;
+            let miss_ = cli_args._miss;
+            let go = cli_args.gap_open;
+            let ge = cli_args.gap_extend;
+            let res = smith_waterman_serial(d, q, match_, miss_, go, ge);
+            println!("{}", res);
+        }
+    }
 }
