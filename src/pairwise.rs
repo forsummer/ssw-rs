dbaf212b95a9965c913bd498de4681082ba8c469
forsummer <1321067378@qq.com>
2022-06-27 20:39:01 +0800

    - Add a direction matrix which store the source of each H-matrix's cell to accelerate traceback - Improve the display format of `AlignResult` - Fixed an issue that caused the values of `d_start` and `q_start` to be incorrect

--- src/pairwise.rs
+++ src/pairwise.rs
@@ -1,19 +1,12 @@
 use std::fmt::Display;
+use std::vec;
 
 use tabular::row;
 use tabular::Table;
 
 use crate::max;
 use crate::load::Seq;
-
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Cell
-{
-    i: usize,
-    j: usize,
-    score: i32,
-}
+use crate::utils::Mat;
 
 pub struct AlignResult
 {
@@ -21,8 +14,11 @@ pub struct AlignResult
     pub q_id: String,
     pub d_start: usize,
     pub q_start: usize,
+    pub d_end: usize,
+    pub q_end: usize,
+    pub d_sub: String,
+    pub q_sub: String,
     pub opt: i32,
-    pub sub_opt: i32,
 }
 
 impl Display for AlignResult
@@ -31,13 +27,24 @@ impl Display for AlignResult
     {
         let table = Table::new("{:<} {:<} {:<}")
             .set_line_end("\n")
-            .with_row(row!("d_id", ":", self.d_id.clone()))
-            .with_row(row!("q_id", ":", self.q_id.clone()))
-            .with_row(row!("d_start", ":", self.d_start))
-            .with_row(row!("q_start", ":", self.q_start))
-            .with_row(row!("opt", ":", self.opt))
-            .with_row(row!("sub_opt", ":", self.sub_opt));
-        write!(f, "{}", &table)
+            .with_row(row!("t_id", ":", &self.d_id))
+            .with_row(row!("q_id", ":", &self.q_id))
+            .with_row(row!("opt", ":", self.opt));
+
+        let mut indication_line = String::new();
+        let d_sub = self.d_sub.clone();
+        let q_sub = self.q_sub.clone();
+        for (r1, r2) in d_sub.chars().zip(q_sub.chars())
+        {
+            let c = if [r1, r2].contains(&'-') {' '} else if r1 == r2 {'|'} else {'*'};
+            indication_line.push(c);
+        }
+
+        let seq = Table::new("{:<} {:<} {:<} {:<} {:<}")
+                        .with_row(row!("target", ":", self.d_start, &self.d_sub, self.d_end))
+                        .with_row(row!("", "", "", indication_line, ""))
+                        .with_row(row!("query", ":", self.q_start, &self.q_sub, self.q_end));
+        write!(f, "{}{}", &table, &seq)
     }
 }
 
@@ -53,15 +60,14 @@ pub fn smith_waterman_serial(d: &Seq, q: &Seq, match_: i32, miss_: i32, go: i32,
     let go = go.abs();
     let ge = ge.abs();
 
-    let mut d_start = 0;
-    let mut q_start = 0;
     let mut prev_e = vec![0; q_len+1];
     let mut prev_h = vec![0; q_len+1];
     let score = |r1, r2| if r1 == r2 { match_ } else { miss_ };
-
+    
     let mut opt = 0;
-    let mut sub_opt = 0;
-    let mut path = Vec::new();
+    let (mut d_start, mut q_start) = (0, 0);
+    let (mut d_end, mut q_end) = (0, 0);
+    let mut direction_mat: Mat<char> = Mat::init((d_len+1, q_len+1));
     for i in 1..d_len+1
     {
         let mut left_f = 0;
@@ -71,25 +77,22 @@ pub fn smith_waterman_serial(d: &Seq, q: &Seq, match_: i32, miss_: i32, go: i32,
         {
             let e = max!(prev_h[j]-go, prev_e[j]-ge, 0);
             let f = max!(left_h-go, left_f-ge, 0);
-            let h = max!(prev_h[j-1]+score(d_seq[i-1], q_seq[j-1]), e, f, 0);
 
-            let mut cell_score = vec![e, f, h];
-            cell_score.sort();
+            let ext = prev_h[j-1] + score(d_seq[i-1], q_seq[j-1]);
+            let h = max!(ext, e, f, 0);
 
-            let tmp_opt = cell_score.pop().unwrap();
-            let tmp_sub = cell_score.pop().unwrap();
+            let tmp_opt = *[e, f, h].iter().max().unwrap();
             if tmp_opt > opt
             {
                 opt = tmp_opt;
-                d_start = i;
-                q_start = j;
-                path.push(Cell { i, j, score: opt });
+                d_end = i;
+                q_end = j;
             }
 
-            if tmp_sub > sub_opt
-            {
-                sub_opt = tmp_sub;
-            }
+            if h == e { direction_mat[i][j] = 'E' }
+            if h == f { direction_mat[i][j] = 'F' }
+            if h == ext { direction_mat[i][j] = 'H' }
+            if h == 0 { direction_mat[i][j] = char::default() }
 
             left_f = f;
             left_h = h;
@@ -98,5 +101,39 @@ pub fn smith_waterman_serial(d: &Seq, q: &Seq, match_: i32, miss_: i32, go: i32,
         }
         prev_h = current_h;
     }
-    AlignResult { d_id, q_id, d_start, q_start, opt, sub_opt }
+
+    let (mut i, mut j) = (d_end, q_end);
+    let mut d_sub  = String::new();
+    let mut q_sub  = String::new();
+    while direction_mat[i][j] != char::default()
+    {
+        if direction_mat[i][j] == 'E'
+        {
+            d_sub.insert(0, d_seq[i-1]);
+            q_sub.insert(0, '-');
+            i = i - 1;
+            d_start = i;
+            continue;
+        }
+
+        if direction_mat[i][j] == 'F'
+        {
+            d_sub.insert(0, '-');
+            q_sub.insert(0, q_seq[j-1]);
+            j = j - 1;
+            q_start = j;
+            continue;
+        }
+
+        if direction_mat[i][j] == 'H'
+        {
+            d_sub.insert(0, d_seq[i-1]);
+            q_sub.insert(0, q_seq[j-1]);
+            (i, j) = (i - 1, j - 1);
+            (d_start, q_start) = (i, j);
+            continue;
+        }
+    }
+    (d_start, q_start) = (d_start + 1, q_start + 1);
+    AlignResult { d_id, q_id, d_start, q_start, d_end, q_end, d_sub, q_sub, opt }
 }
