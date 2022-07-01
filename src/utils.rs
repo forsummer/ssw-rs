42d8ad89e463a2a929ebe048f450dbca63b048b2
forsummer <1321067378@qq.com>
2022-07-01 12:52:35 +0800

    - Put `matrix` operation set and `AVX/AVX2` operation set into two module which name `mat` and `AVX` - Move code about `AlignResult` to `pairwise` module

--- src/utils.rs
+++ src/utils.rs
@@ -1,307 +1,272 @@
-use std::ops::Add;
-use std::ops::Sub;
-use std::ops::Shl;
-use std::ops::Index;
-use std::ops::IndexMut;
-use std::mem::transmute;
-use std::mem::size_of;
-use std::fmt::Debug;
-use std::fmt::Display;
-use std::arch::x86_64::__m256i;
-use std::arch::x86_64::_mm256_add_epi32;
-use std::arch::x86_64::_mm256_sub_epi32;
-use std::arch::x86_64::_mm256_set_epi32;
-use std::arch::x86_64::_mm256_setr_epi32;
-use std::arch::x86_64::_mm256_set1_epi32;
-use std::arch::x86_64::_mm256_cmpeq_epi32;
-use std::arch::x86_64::_mm256_cmpgt_epi32;
-use std::arch::x86_64::_mm256_movemask_epi8;
-use std::arch::x86_64::_mm256_setzero_si256;
-
-use tabular::row;
-use tabular::Table;
-
-pub struct AlignResult
+pub mod matrix
 {
-    pub d_id: String,
-    pub q_id: String,
-    pub d_start: usize,
-    pub q_start: usize,
-    pub d_end: usize,
-    pub q_end: usize,
-    pub d_sub: String,
-    pub q_sub: String,
-    pub opt: i32,
-}
+    use std::ops::Index;
+    use std::ops::IndexMut;
 
-impl Display for AlignResult
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    pub struct Mat<T>
+    where
+        T: Copy + Clone + Sized + Default
     {
-        let table = Table::new("{:<} {:<} {:<}")
-            .set_line_end("\n")
-            .with_row(row!("t_id", ":", &self.d_id))
-            .with_row(row!("q_id", ":", &self.q_id))
-            .with_row(row!("opt", ":", self.opt));
+        mat: Vec<Vec<T>>,
+        pub shape: (usize, usize),
+    }
 
-        let mut indication_line = String::new();
-        let d_sub = self.d_sub.clone();
-        let q_sub = self.q_sub.clone();
-        for (r1, r2) in d_sub.chars().zip(q_sub.chars())
+    impl<T> Mat<T>
+    where
+        T: Copy + Clone + Sized + Default
+    {
+        pub fn init(shape: (usize, usize)) -> Mat<T>
         {
-            let c = if [r1, r2].contains(&'-') {' '} else if r1 == r2 {'|'} else {'*'};
-            indication_line.push(c);
+            let mat = vec![vec![T::default(); shape.1]; shape.0];
+            Mat { mat, shape }
         }
-
-        let seq = Table::new("{:<} {:<} {:<} {:<} {:<}")
-                        .with_row(row!("target", ":", self.d_start, &self.d_sub, self.d_end))
-                        .with_row(row!("", "", "", indication_line, ""))
-                        .with_row(row!("query", ":", self.q_start, &self.q_sub, self.q_end));
-        write!(f, "{}{}", &table, &seq)
-    }
-}
-
-pub struct Mat<T>
-where
-    T: Copy + Clone + Sized + Default
-{
-    mat: Vec<Vec<T>>,
-    pub shape: (usize, usize),
-}
-
-impl<T> Mat<T>
-where
-    T: Copy + Clone + Sized + Default
-{
-    pub fn init(shape: (usize, usize)) -> Mat<T>
-    {
-        let mat = vec![vec![T::default(); shape.1]; shape.0];
-        Mat { mat, shape }
-    }
-}
-
-impl<T> Index<usize> for Mat<T>
-where
-    T: Copy + Clone + Sized + Default
-{
-    type Output = Vec<T>;
-    fn index(&self, index: usize) -> &Self::Output
-    {
-        &self.mat[index]
-    }
-}
-
-impl<T> IndexMut<usize> for Mat<T>
-where
-    T: Copy + Clone + Sized + Default
-{
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output
-    {
-        &mut self.mat[index]
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct M256Epi32(pub __m256i);
-
-impl PartialEq for M256Epi32
-{
-    fn eq(&self, other: &Self) -> bool
-    {
-        unsafe { _mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1 }
     }
 
-    fn ne(&self, other: &Self) -> bool
+    impl<T> Index<usize> for Mat<T>
+    where
+        T: Copy + Clone + Sized + Default
     {
-        !unsafe { _mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1 }
-    }
-}
-
-impl PartialOrd for M256Epi32
-{
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
-    {
-        if unsafe {_mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1}
+        type Output = Vec<T>;
+        fn index(&self, index: usize) -> &Self::Output
         {
-            Some(std::cmp::Ordering::Equal)
+            &self.mat[index]
         }
-        else if unsafe {_mm256_movemask_epi8(_mm256_cmpgt_epi32(self.0, other.0)) == -1}
+    }
+
+    impl<T> IndexMut<usize> for Mat<T>
+    where
+        T: Copy + Clone + Sized + Default
+    {
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output
         {
-            Some(std::cmp::Ordering::Greater)
-        }
-        else if unsafe {_mm256_movemask_epi8(_mm256_cmpgt_epi32(other.0, self.0)) == -1}
-        {
-            Some(std::cmp::Ordering::Less)
-        }
-        else
-        {
-            None
+            &mut self.mat[index]
         }
     }
 }
 
-impl Add for M256Epi32
+pub mod avx
 {
-    type Output = M256Epi32;
-    fn add(self, rhs: Self) -> Self::Output
-    {
-        unsafe { M256Epi32(_mm256_add_epi32(self.0, rhs.0)) }
-    }
-}
+    use std::ops::Add;
+    use std::ops::Sub;
+    use std::ops::Shl;
+    use std::ops::Index;
+    use std::fmt::Debug;
+    use std::fmt::Display;
+    use std::mem::transmute;
+    use std::mem::size_of;
+    use std::arch::x86_64::__m256i;
+    use std::arch::x86_64::_mm256_add_epi32;
+    use std::arch::x86_64::_mm256_sub_epi32;
+    use std::arch::x86_64::_mm256_set_epi32;
+    use std::arch::x86_64::_mm256_setr_epi32;
+    use std::arch::x86_64::_mm256_set1_epi32;
+    use std::arch::x86_64::_mm256_cmpeq_epi32;
+    use std::arch::x86_64::_mm256_cmpgt_epi32;
+    use std::arch::x86_64::_mm256_movemask_epi8;
+    use std::arch::x86_64::_mm256_setzero_si256;
 
-impl Sub for M256Epi32
-{
-    type Output = M256Epi32;
-    fn sub(self, rhs: Self) -> Self::Output
-    {
-        unsafe { M256Epi32(_mm256_sub_epi32(self.0, rhs.0)) }
-    }
-}
+    #[derive(Clone, Copy)]
+    pub struct M256Epi32(pub __m256i);
 
-impl Shl<usize> for M256Epi32
-{
-    type Output = M256Epi32;
-    fn shl(self, rhs: usize) -> Self::Output
+    impl PartialEq for M256Epi32
     {
-        if rhs > 7
+        fn eq(&self, other: &Self) -> bool
         {
-            panic!("Out of bound");
+            unsafe { _mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1 }
         }
-        unsafe
+
+        fn ne(&self, other: &Self) -> bool
         {
-            let out = transmute::<*const __m256i, *mut i32>(&_mm256_setzero_si256() as *const __m256i);
-            let ptr = transmute::<*const __m256i, *const i32>(&self.0 as *const __m256i).add(rhs);
-            ptr.copy_to(out, (8 - rhs) * size_of::<i32>());
-            M256Epi32(*transmute::<*mut i32, *const __m256i>(out))
-        }
-    }
-}
-
-impl Index<usize> for M256Epi32
-{
-    type Output = i32;
-    fn index(&self, index: usize) -> &Self::Output
-    {
-        if index > 7
-        {
-            panic!("Out of bound");
-        }
-        let M256Epi32(arr) = self;
-        unsafe
-        {
-            let ptr_arr = transmute::<*const __m256i, *const __m256i>(arr as *const __m256i);
-            &*transmute::<*const __m256i, *const i32>(ptr_arr).add(index)
-        }
-    }
-}
-
-impl Display for M256Epi32
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-    {
-        write!(f, "[{}, {}, {}, {}, {}, {}, {}, {}]", 
-        self[0], self[1], self[2], self[3], self[4], self[5], self[6], self[7])
-    }
-}
-
-impl Debug for M256Epi32
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-    {
-        f.debug_list()
-            .entry(&self[0])
-            .entry(&self[1])
-            .entry(&self[2])
-            .entry(&self[3])
-            .entry(&self[4])
-            .entry(&self[5])
-            .entry(&self[6])
-            .entry(&self[7])
-            .finish()
-    }
-}
-
-#[allow(dead_code)]
-impl M256Epi32
-{
-    pub fn zero() -> M256Epi32
-    {
-        unsafe { M256Epi32(_mm256_setzero_si256()) }
-    }
-
-    pub fn set(e0: i32, e1: i32, e2: i32, e3: i32, e4: i32, e5: i32, e6: i32, e7: i32) -> M256Epi32
-    {
-        unsafe { M256Epi32(_mm256_set_epi32(e0, e1, e2, e3, e4, e5, e6, e7)) }
-    }
-
-    pub fn setr(e0: i32, e1: i32, e2: i32, e3: i32, e4: i32, e5: i32, e6: i32, e7: i32) -> M256Epi32
-    {
-        unsafe { M256Epi32(_mm256_setr_epi32(e0, e1, e2, e3, e4, e5, e6, e7)) }
-    }
-
-    pub fn fill(item: i32) -> M256Epi32
-    {
-        unsafe { M256Epi32(_mm256_set1_epi32(item)) }
-    }
-
-    pub fn from_arr(arr: &[i32; 8]) -> M256Epi32
-    {
-        unsafe { M256Epi32(*transmute::<*const i32, *const __m256i>(arr.as_ptr())) }
-    }
-
-    pub fn from_vec(vec: &Vec<i32>) -> M256Epi32
-    {
-        if vec.len() != 8 { panic!("The length of vec should equal to 8") }
-        unsafe { M256Epi32(*transmute::<*const i32, *const __m256i>(vec.as_ptr())) }
-    }
-
-    pub fn to_arr(&self) -> [i32; 8]
-    {
-        unsafe
-        {
-            let mut arr = [0; 8];
-            let ptr = transmute::<*const __m256i, *const i32>(&(self.0) as *const __m256i);
-            ptr.copy_to(arr.as_mut_ptr(), 8);
-            arr
+            !unsafe { _mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1 }
         }
     }
 
-    pub fn to_vec(&self) -> Vec<i32>
+    impl PartialOrd for M256Epi32
     {
-        unsafe
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
         {
-            let mut vec = vec![0; 8];
-            let ptr = transmute::<*const __m256i, *const i32>(&self.0 as *const __m256i);
-            ptr.copy_to(vec.as_mut_ptr(), 8);
-            vec
-        }
-    }
-
-    pub fn get_max_m256_i32(&self) -> i32
-    {
-        let mut arr = self.to_arr();
-        arr.sort();
-        *(arr.last().unwrap())
-    }
-}
-
-#[macro_export]
-macro_rules! max_epi32
-{
-    ( $ ( $arr: expr ), * ) => 
-    { 
-        {
-            let mut max = M256Epi32::zero();
-            use std::arch::x86_64::_mm256_max_epi32;
-            let max_arr = |a: M256Epi32, b: M256Epi32| 
+            if unsafe {_mm256_movemask_epi8(_mm256_cmpeq_epi32(self.0, other.0)) == -1}
             {
-                let (M256Epi32(x), M256Epi32(y)) = (a, b);
-                unsafe { _mm256_max_epi32(x, y) }
-            };
-            $( max = M256Epi32(max_arr(max, $arr)); )*
-            max
+                Some(std::cmp::Ordering::Equal)
+            }
+            else if unsafe {_mm256_movemask_epi8(_mm256_cmpgt_epi32(self.0, other.0)) == -1}
+            {
+                Some(std::cmp::Ordering::Greater)
+            }
+            else if unsafe {_mm256_movemask_epi8(_mm256_cmpgt_epi32(other.0, self.0)) == -1}
+            {
+                Some(std::cmp::Ordering::Less)
+            }
+            else
+            {
+                None
+            }
         }
-    };
+    }
+
+    impl Add for M256Epi32
+    {
+        type Output = M256Epi32;
+        fn add(self, rhs: Self) -> Self::Output
+        {
+            unsafe { M256Epi32(_mm256_add_epi32(self.0, rhs.0)) }
+        }
+    }
+
+    impl Sub for M256Epi32
+    {
+        type Output = M256Epi32;
+        fn sub(self, rhs: Self) -> Self::Output
+        {
+            unsafe { M256Epi32(_mm256_sub_epi32(self.0, rhs.0)) }
+        }
+    }
+
+    impl Shl<usize> for M256Epi32
+    {
+        type Output = M256Epi32;
+        fn shl(self, rhs: usize) -> Self::Output
+        {
+            if rhs > 7
+            {
+                panic!("Out of bound");
+            }
+            unsafe
+            {
+                let out = transmute::<*const __m256i, *mut i32>(&_mm256_setzero_si256() as *const __m256i);
+                let ptr = transmute::<*const __m256i, *const i32>(&self.0 as *const __m256i).add(rhs);
+                ptr.copy_to(out, (8 - rhs) * size_of::<i32>());
+                M256Epi32(*transmute::<*mut i32, *const __m256i>(out))
+            }
+        }
+    }
+
+    impl Index<usize> for M256Epi32
+    {
+        type Output = i32;
+        fn index(&self, index: usize) -> &Self::Output
+        {
+            if index > 7
+            {
+                panic!("Out of bound");
+            }
+            let M256Epi32(arr) = self;
+            unsafe
+            {
+                let ptr_arr = transmute::<*const __m256i, *const __m256i>(arr as *const __m256i);
+                &*transmute::<*const __m256i, *const i32>(ptr_arr).add(index)
+            }
+        }
+    }
+
+    impl Display for M256Epi32
+    {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+        {
+            write!(f, "[{}, {}, {}, {}, {}, {}, {}, {}]", 
+            self[0], self[1], self[2], self[3], self[4], self[5], self[6], self[7])
+        }
+    }
+
+    impl Debug for M256Epi32
+    {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+        {
+            f.debug_list()
+                .entry(&self[0])
+                .entry(&self[1])
+                .entry(&self[2])
+                .entry(&self[3])
+                .entry(&self[4])
+                .entry(&self[5])
+                .entry(&self[6])
+                .entry(&self[7])
+                .finish()
+        }
+    }
+
+    #[allow(dead_code)]
+    impl M256Epi32
+    {
+        pub fn zero() -> M256Epi32
+        {
+            unsafe { M256Epi32(_mm256_setzero_si256()) }
+        }
+
+        pub fn set(e0: i32, e1: i32, e2: i32, e3: i32, e4: i32, e5: i32, e6: i32, e7: i32) -> M256Epi32
+        {
+            unsafe { M256Epi32(_mm256_set_epi32(e0, e1, e2, e3, e4, e5, e6, e7)) }
+        }
+
+        pub fn setr(e0: i32, e1: i32, e2: i32, e3: i32, e4: i32, e5: i32, e6: i32, e7: i32) -> M256Epi32
+        {
+            unsafe { M256Epi32(_mm256_setr_epi32(e0, e1, e2, e3, e4, e5, e6, e7)) }
+        }
+
+        pub fn fill(item: i32) -> M256Epi32
+        {
+            unsafe { M256Epi32(_mm256_set1_epi32(item)) }
+        }
+
+        pub fn from_arr(arr: &[i32; 8]) -> M256Epi32
+        {
+            unsafe { M256Epi32(*transmute::<*const i32, *const __m256i>(arr.as_ptr())) }
+        }
+
+        pub fn from_vec(vec: &Vec<i32>) -> M256Epi32
+        {
+            if vec.len() != 8 { panic!("The length of vec should equal to 8") }
+            unsafe { M256Epi32(*transmute::<*const i32, *const __m256i>(vec.as_ptr())) }
+        }
+
+        pub fn to_arr(&self) -> [i32; 8]
+        {
+            unsafe
+            {
+                let mut arr = [0; 8];
+                let ptr = transmute::<*const __m256i, *const i32>(&(self.0) as *const __m256i);
+                ptr.copy_to(arr.as_mut_ptr(), 8);
+                arr
+            }
+        }
+
+        pub fn to_vec(&self) -> Vec<i32>
+        {
+            unsafe
+            {
+                let mut vec = vec![0; 8];
+                let ptr = transmute::<*const __m256i, *const i32>(&self.0 as *const __m256i);
+                ptr.copy_to(vec.as_mut_ptr(), 8);
+                vec
+            }
+        }
+
+        pub fn get_max_m256_i32(&self) -> i32
+        {
+            let mut arr = self.to_arr();
+            arr.sort();
+            *(arr.last().unwrap())
+        }
+    }
+
+    #[macro_export]
+    macro_rules! max_epi32
+    {
+        ( $ ( $arr: expr ), * ) => 
+        { 
+            {
+                let mut max = M256Epi32::zero();
+                use std::arch::x86_64::_mm256_max_epi32;
+                let max_arr = |a: M256Epi32, b: M256Epi32| 
+                {
+                    let (M256Epi32(x), M256Epi32(y)) = (a, b);
+                    unsafe { _mm256_max_epi32(x, y) }
+                };
+                $( max = M256Epi32(max_arr(max, $arr)); )*
+                max
+            }
+        };
+    }
 }
 
 #[macro_export]
@@ -317,7 +282,8 @@ macro_rules! max
 #[cfg(test)]
 mod test
 {
-    use super::*;
+    use crate::max_epi32;
+    use super::avx::M256Epi32;
 
     #[test]
     fn test_eq()
