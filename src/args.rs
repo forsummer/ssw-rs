18e8748cb1319ef11a8fd131df2010d82c99cc08
forsummer <1321067378@qq.com>
2022-07-09 20:42:05 +0800

    Parsing `miss` and `match` score to `i16` type instead `u16` type

--- src/args.rs
+++ src/args.rs
@@ -16,30 +16,36 @@ pub struct CliArgs
     #[clap(long)]
     #[clap(default_value_t = 1)]
     #[clap(parse(try_from_str = is_integers))]
-    #[clap(help = "pair residue matching score (positive integer)")]
-    pub _match: u32,
+    #[clap(help = "pair residue matching score (integer: [-32768..32767])")]
+    pub _match: i16,
 
     #[clap(long)]
     #[clap(default_value_t = 1)]
     #[clap(parse(try_from_str = is_integers))]
-    #[clap(help = "pair residue miss match score (positive integer)")]
-    pub _miss: u32,
+    #[clap(help = "pair residue miss match score (integer: [-32768..32767])")]
+    pub _miss: i16,
 
     #[clap(long)]
     #[clap(default_value_t = 3)]
-    #[clap(parse(try_from_str = is_integers))]
-    #[clap(help = "gap open score (positive integer)")]
-    pub gap_open: u32,
+    #[clap(parse(try_from_str = is_non_negative_integers))]
+    #[clap(help = "gap open score (positive integer: [0..65535])")]
+    pub gap_open: u16,
 
     #[clap(long)]
     #[clap(default_value_t = 2)]
-    #[clap(parse(try_from_str = is_integers))]
-    #[clap(help = "gap extend score (positive integer)")]
-    pub gap_extend: u32,
+    #[clap(parse(try_from_str = is_non_negative_integers))]
+    #[clap(help = "gap extend score (positive integer: [0..65535])")]
+    pub gap_extend: u16,
 }
 
-fn is_integers(arg: &str) -> Result<u32, String>
+fn is_integers(arg: &str) -> Result<i16, String>
 {
-    let score: u32 = arg.parse().map_err(|_expection| format!("{} not a integers", arg))?;
+    let score: i16 = arg.parse().map_err(|_expection| format!("{} not a integers", arg))?;
+    Ok(score)
+}
+
+fn is_non_negative_integers(arg: &str) -> Result<u16, String>
+{
+    let score: u16 = arg.parse().map_err(|_expection| format!("{} not a Non-Negative integer", arg))?;
     Ok(score)
 }
